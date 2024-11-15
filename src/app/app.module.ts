22eebb430844e1b5c56d27e7b25dcbdbf77280b2
Anna Mika <a.mika@accenture.com>
2024-11-15 15:17:45 +0100

    content-container created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { ContentContainerComponent } from './content-container/content-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    ContentContainerComponent
   ],
   imports: [
     BrowserModule
